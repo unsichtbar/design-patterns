6609fe0db9ef31bff7264415fd09876921aede94
Alex Lindemann <exmann94@gmail.com>
2021-07-01 18:37:07 -0500

    dispatcher example

---
+++ dispatcher/src/main/java/com/example/dispatcher/DispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.dispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatcherApplication.class, args);
+	}
+
+}
